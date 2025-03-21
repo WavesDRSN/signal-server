eee152e417d76890d4078287701cebf6616f31e7
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-03-22 01:29:42 +0300

    Реализован UserRepository для работы с пользователями

--- src/main/java/wavesDRSN/p2p_messenger_backend/repositories/UserRepository.java
+++ src/main/java/wavesDRSN/p2p_messenger_backend/repositories/UserRepository.java
@@ -1,10 +1,25 @@
 package wavesDRSN.p2p_messenger_backend.repositories;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import wavesDRSN.p2p_messenger_backend.entities.UserEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<SecurityProperties.User, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByUsername(String username); // поиск по уникальному имени юзера
+
+    List<UserEntity> findByPublicName(String publicName); // поиск по публ имени
+
+    List<UserEntity> findByIsOnline(boolean isOnline); // поиск пользователей по статусу
+
+    List<UserEntity> findByCreatedAtAfter(LocalDateTime date); // поиск пользователей, зарегистрированных после указанной даты
+
+    List<UserEntity> findByLastActiveAtBefore(LocalDateTime date); // {опционально} поиск юзеров, которые не заходили в сеть после указаной даты
+    // например пользователь неактивен 2 недели
+
 
 }
