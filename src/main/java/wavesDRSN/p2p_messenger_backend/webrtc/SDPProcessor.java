af34f4490dbe4fd442f3185162ab116473e61987
HankyYt <timgurov@yandex.ru>
2025-03-28 22:44:46 +0300

    PD-22 Изменить файлы под структуру proto-файла и изменить версию спринг бута

--- src/main/java/wavesDRSN/p2p_messenger_backend/webrtc/SDPProcessor.java
+++ src/main/java/wavesDRSN/p2p_messenger_backend/webrtc/SDPProcessor.java
@@ -2,14 +2,19 @@ package wavesDRSN.p2p_messenger_backend.webrtc;
 
 import gRPC.v1.SessionDescription;
 import org.springframework.stereotype.Component;
+import wavesDRSN.p2p_messenger_backend.session.UserSessionManager;
 
 @Component
 public class SDPProcessor {
-    public void processOffer(SessionDescription offer) {
-        // Логика создания SDP answer
+    private final UserSessionManager sessionManager;
+
+    public SDPProcessor(UserSessionManager sessionManager) {
+        this.sessionManager = sessionManager;
     }
 
-    public void processAnswer(SessionDescription answer) {
-        // Логика применения SDP answer
+    public void processSDP(SessionDescription sdp) {
+        sessionManager.getSession(sdp.getReceiver()).ifPresent(receiver ->
+            receiver.sendSDP(sdp)
+        );
     }
 }
