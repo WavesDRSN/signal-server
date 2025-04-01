e3093999b67834dda4deea0eeac24d73c98bb035
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-04-02 02:32:05 +0300

    Изменить название login на username(я так вижу, я художник)

--- db/migration/V1__Create_users_table.sql
+++ db/migration/V1__Create_users_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE users (
                        id BIGSERIAL PRIMARY KEY, -- автоинкрементный уник ID
-                       login VARCHAR(32) NOT NULL, -- логин
+                       username VARCHAR(32) NOT NULL, -- логин
                        public_key BYTEA NOT NULL UNIQUE, -- публичный ключ
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- дата регистрации юзера
 
     -- Ограничения
-                       CONSTRAINT login_length CHECK (LENGTH(login) BETWEEN 5 AND 32),
-                       CONSTRAINT login_format CHECK (login ~ '^[a-zA-Z0-9_]+$')
+                       CONSTRAINT login_length CHECK (LENGTH(username) BETWEEN 5 AND 32),
+                       CONSTRAINT login_format CHECK (username ~ '^[a-zA-Z0-9_]+$')
     );
 
 -- Регистронезависимая уникальность логина (trippi_troppi = Trippi_Troppi = TRIPPI_TROPPI и тд)
 -- Ник сохраняется в БД как введен пользователем.
 CREATE UNIQUE INDEX idx_users_lower_login
-    ON users (LOWER(login));
+    ON users (LOWER(username));
