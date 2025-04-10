f374e259e682fd27d2eb45e124acffc03a674099
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-04-11 00:30:46 +0300

    Добавить адекватное логирование с помощью аннотации @Slf4j

--- src/main/java/wavesDRSN/p2p_messenger_backend/services/CryptographyServiceImpl.java
+++ src/main/java/wavesDRSN/p2p_messenger_backend/services/CryptographyServiceImpl.java
@@ -1,11 +1,13 @@
 package wavesDRSN.p2p_messenger_backend.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 
+@Slf4j
 @Service
 public class CryptographyServiceImpl implements CryptographyService {
 
@@ -15,18 +17,14 @@ public class CryptographyServiceImpl implements CryptographyService {
             KeyFactory keyFactory = KeyFactory.getInstance("Ed25519", "BC"); // создает ключ из байт
             X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyDerBytes); // для распознавания ключа
             return keyFactory.generatePublic(keySpec); // возвращает готовый PublicKey
-        }
-        catch (NoSuchAlgorithmException e) {
-            System.err.println("Алгоритм не поддерживается" + e.getMessage());
-        }
-        catch (NoSuchProviderException e) {
-            System.err.println("Провайдер не найден" + e.getMessage());
-        }
-        catch (InvalidKeySpecException e) {
-            System.err.println("Неверная спецификация ключа" + e.getMessage());
-        }
-        catch (Exception e) {
-            System.err.println("Произошла непредвиденная ошибка при загрузке ключа" + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Алгоритм не поддерживается: {}", e.getMessage());
+        } catch (NoSuchProviderException e) {
+            log.error("Провайдер не найден: {}", e.getMessage());
+        } catch (InvalidKeySpecException e) {
+            log.error("Неверная спецификация ключа: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Произошла непредвиденная ошибка при загрузке ключа: {}", e.getMessage());
         }
         return null;
     }
@@ -38,21 +36,16 @@ public class CryptographyServiceImpl implements CryptographyService {
             signature.initVerify(publicKey);
             signature.update(data);
             return signature.verify(signatureBytes);
-        }
-        catch (NoSuchAlgorithmException e) {
-            System.err.println("Алгоритм не поддерживается" + e.getMessage());
-        }
-        catch (NoSuchProviderException e) {
-            System.err.println("Провайдер не найден" + e.getMessage());
-        }
-        catch (InvalidKeyException e) {
-            System.err.println("Недопустимый публичный ключ" + e.getMessage());
-        }
-        catch (SignatureException e) {
-            System.err.println("Ошибка при обработке подписи" + e.getMessage());
-        }
-        catch (Exception e) {
-            System.err.println("Непредвиденная ошибка" + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            log.error("Алгоритм Ed25519 не поддерживается: {}", e.getMessage(), e);
+        } catch (NoSuchProviderException e) {
+            log.error("Провайдер Bouncy Castle не найден: {}", e.getMessage(), e);
+        } catch (InvalidKeyException e) {
+            log.error("Недопустимый публичный ключ: {}", e.getMessage(), e);
+        } catch (SignatureException e) {
+            log.error("Ошибка при обработке подписи: {}", e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("Непредвиденная ошибка: {}", e.getMessage(), e);
         }
         return false;
     }
