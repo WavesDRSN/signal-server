0a1aa7cd51791bbe0cfaddd1f1c7f2990182265d
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-04-15 23:38:18 +0300

    Добавить пояснения

--- src/main/java/wavesDRSN/p2p_messenger_backend/security/ChallengeCache.java
+++ src/main/java/wavesDRSN/p2p_messenger_backend/security/ChallengeCache.java
@@ -3,7 +3,18 @@ package wavesDRSN.p2p_messenger_backend.security;
 import java.time.Duration;
 
 public interface ChallengeCache  {
+    /**
+     *  Сохраняет challenge с TTL
+     * @param challengeId идентификатор
+     * @param challenge байты challenge
+     * @param TTL время жизни
+     */
     void putChallenge(String challengeId, byte[] challenge, Duration TTL);
 
+    /**
+     * Получает и удаляет challenge по идентификатору
+     * @param challengeId идентификатор
+     * @return байты или null
+     */
     byte[] getAndRemove(String challengeId);
 }
