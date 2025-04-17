991ea725a5c56a4ef6085ecee3e6440b685a78f9
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-04-17 22:50:41 +0300

    Реализовать интерфейс ChallengeCache

--- src/main/java/wavesDRSN/p2p_messenger_backend/security/ChallengeCacheImpl.java
+++ src/main/java/wavesDRSN/p2p_messenger_backend/security/ChallengeCacheImpl.java
@@ -1,19 +1,38 @@
 package wavesDRSN.p2p_messenger_backend.security;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
+import org.springframework.util.StringUtils;
 
 @Service
+@RequiredArgsConstructor
 public class ChallengeCacheImpl implements ChallengeCache {
+    private final Cache<String, byte[]> cacheStore;
+
+    // добавляет запись в кэш
     @Override
-    public void putChallenge(String challengeId, byte[] challenge, Duration TTL) {
-        //TODO: заглушка
+    public void putChallenge(String challengeId, byte[] challenge) {
+        if (!StringUtils.hasText(challengeId)) {
+            throw new IllegalArgumentException("challengeId не может быть пустым");
+        }
+
+        if (challenge == null || challenge.length == 0) {
+            throw new IllegalArgumentException("Массив байт challenge не может быть null или пустым для ID: \" + challengeId)");
+        }
+
+        cacheStore.put(challengeId, challenge);
+        // log.debug("Challenge сохранен для ID: {}", challengeId); // опциональное логирование
     }
 
+    // получает и удаляет challenge из кэша по ID
     @Override
     public byte[] getAndRemove(String challengeId) {
-        return null; //TODO: заглушка
+        byte[] challenge = cacheStore.getIfPresent(challengeId);
+        if (challenge != null) {
+            cacheStore.invalidate(challengeId);
+        }
+        return challenge;
     }
 
 }
