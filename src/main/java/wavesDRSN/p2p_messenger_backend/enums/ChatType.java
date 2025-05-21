f7d23f081f29820a7fa29f92e52db4d8791c8bba
Артем Алексеевич Дубов <dubov_2005@mail.ru>
2025-05-21 13:18:07 +0300

    Удалить маленькие пуки

--- src/main/java/wavesDRSN/p2p_messenger_backend/enums/ChatType.java
+++
@@ -1,7 +0,0 @@
-package wavesDRSN.p2p_messenger_backend.enums;
-
-// типы чатов
-public enum ChatType {
-    PERSONAL,
-    GROUP
-}
--- src/main/java/wavesDRSN/p2p_messenger_backend/enums/MessageStatus.java
+++
@@ -1,7 +0,0 @@
-package wavesDRSN.p2p_messenger_backend.enums;
-
-public enum MessageStatus {
-    SENT,
-    DELIVERED,
-    READ
-}
